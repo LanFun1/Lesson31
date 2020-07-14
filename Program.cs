f8fd8a0ed70dd4cb3eee14857e0388936a2e3711
LanFun1 <66485374+LanFun1@users.noreply.github.com>
2020-07-14 10:40:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = { 
+                { 3, 4, 5, 7 }, 
+                { 4, 5, 6, 7 }, 
+                { 4, 7, 9, 2 } };
+            int sumLine = 0;
+            int sumColumn = 0;
+            for (int i = 0; i < array.GetLength(1); i++)
+            {
+                sumLine += array[1, i];
+            }
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                sumColumn += array[i, 0];
+            }
+            Console.WriteLine($"Сумма второй строки: {sumLine}\nСумма первого столбца: {sumColumn}");
+        }
+    }
+}
